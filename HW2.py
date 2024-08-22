5487761505e91622babc2c7880ce0b4b7d669e81
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-08-22 13:38:51 -0500

    added HW2

---
+++ HW2.py
@@ -0,0 +1,15 @@
+# Name : Evan Pagliasotti
+#Hour:Hour 6
+#assignment:HW2
+
+
+print ("hello world")
+
+input("what is your name")
+
+x=int(input("give me a interger"))
+
+y=int(input("give me another iterger"))
+
+z=x+y
+print(z)
