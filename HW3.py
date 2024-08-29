65b5409bb241157098f5f166d771476e60491dc0
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-08-29 13:41:34 -0500

    added HW3

---
+++ HW3.py
@@ -0,0 +1,28 @@
+#Name: Evan J Pagliasotti
+# Class: Hour 6
+# Assignment: HW3
+# Help FFrom Google
+
+
+
+
+
+
+print ("hello world")
+
+#people list
+
+people= ["dave","jim","john","harvy","evan"]
+people. append('jimmothy')
+print (people)
+
+print( people[3])
+
+numbers=[ 36,98238,2929,10389]
+print (numbers)
+numbers.insert(1,2673682)
+numbers.sort()
+print (numbers)
+
+variable=(numbers[0]+numbers[1]+numbers[2])
+print (variable)
