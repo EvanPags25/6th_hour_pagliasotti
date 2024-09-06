4113af1d3496aa7345b4dca3018def866d502b02
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-09-06 13:58:13 -0500

    added playground

---
+++ playground.py
@@ -0,0 +1,24 @@
+#Name: Evan Pagliasotti
+#Hour: Hour 6
+#Assignment: Playground
+
+#hello print
+print('hello')
+
+#calculater and name user inputs
+input('what is your name?')
+x=int(input("enter a interger"))
+y=int(input("enter another interger"))
+z=x+y
+print('answer')
+print(z)
+
+#print moving on
+print('moving on')
+
+#numbers
+numbers=[230849,893042,10,842]
+print(numbers)
+
+#name list
+names=(
