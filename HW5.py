8be9309f21b35d762a94a893a3b9844bfaa8097a
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-09-27 14:03:22 -0500

    added HW5

---
+++ HW5.py
@@ -0,0 +1,26 @@
+#Name: Evan Pagliasotti
+#Class: HR6
+#Assignment: HW5
+
+print("hello world")
+
+a=4
+b=6
+c=22
+print("A=4, B=6, C=22")
+
+if a > b:
+    print("a is greater than b.")
+elif a < b:
+    print("a is less than b.")
+else:
+    print('a is equal to b.')
+
+
+if a<b or a>c:
+    print("a is NOT the smallest number")
+if b<a or b<c:
+    print("b is the smallest number")
+else:
+    print("hello world")
+
