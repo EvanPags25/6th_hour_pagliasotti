8a3024ed204a8d2e2b9fdad3f38ffad1cd1d754a
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-10-24 13:58:45 -0500

    added HW9

---
+++ HW9.py
@@ -0,0 +1,22 @@
+#Name: Evan Pagliasotti
+#Hour:HR6
+#Assignment:HW9
+
+
+
+
+#1. Print Hello World!
+print("hello world")
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+temp=15
+
+if temp>20:
+    print("it's hot"),
+elif temp>10:
+    print("it's mild")
+else:
+    print("its cold")
+
+print("thank you")
