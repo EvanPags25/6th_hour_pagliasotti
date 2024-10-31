6a984326932d8a7fb4550bf426ed4c0fa09e40f6
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-10-31 13:34:20 -0500

    added HW10

---
+++ HW10.py
@@ -0,0 +1,29 @@
+#Name: Evan Pagliasotti
+#Class: 6th Hour
+#Assignment: HW10
+#help from: stackoverflow.com and programiz.com
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+i=0
+while i <= 30:
+    print(i)
+    i += 1
+print("hello world")
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+i=2
+while i <= 30:
+    if ( i % 2==0):
+        print (i,)
+    i+=1
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
+
+# Print numbers until the user enters 0
+number = int(input('Enter a number: '))
+
+# iterate until the user enters 0
+while number != 0:
+    print(f'You entered {number}.')
+    number = int(input('Enter a number: '))
