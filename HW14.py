c0626cb864d22ca12ab361e32a99e13cfd6eeedb
Evan Pagliasotti <ep127007@student.wpsok.org>
2024-11-13 14:08:56 -0600

    added HW14

---
+++ HW14.py
@@ -0,0 +1,18 @@
+#Name: Evan Pagliasotti
+#Class: 6th Hour
+#Assignment: HW14
+
+
+#1. Create a variable that asks the user for an integer and an empty intger variable.
+number = int(input('Enter a number:'))
+x=1
+#2. Create a loop with a range from 1 to the number the user input.
+for i in range (1,number +1):
+    x*=i
+print(x)
+
+#3. Use the loop to find the factorial of that number. A factorial of a number is that number multiplied
+#by every number before it until you reach 1.
+#For example: 5! is 5 x 4 x 3 x 2 x 1 = 120
+
+#4. Print the factorial.
